98045106cccd2b457b4e7e4590ae70c2ebd3ee03
laptevusha <87438254+laptevusha@users.noreply.github.com>
2021-07-14 22:11:15 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+FILE_NAME = '123.xlsx'#Имя файла(должен находиться в той же директори, что и бот
+
+FIELD_NAME = 'ITEM'#Поле для поиска
+
+TOKEN = '1619442200:AAHQEb78I_wabHoBYD2fIS0u5Wb8ubMW3o0'
+
+SHEET = 'Лист1'
