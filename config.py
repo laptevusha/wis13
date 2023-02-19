49f48136f4071a57577d2126f24c7cfc9f41919c
laptevusha <87438254+laptevusha@users.noreply.github.com>
2023-02-19 19:52:27 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-FILE_NAME = '123.xlsx'#Имя файла(должен находиться в той же директори, что и бот
-
-FIELD_NAME = 'ITEM'#Поле для поиска
-
-TOKEN = '6021283271:AAHf99148Uh0qXQ2eweCNFb43ceLw3T6_Ts'
-
-SHEET = 'Лист1'
