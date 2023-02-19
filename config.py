f9e8f274347f052d4c3a7a697c4d6165bd63acc7
laptevusha <87438254+laptevusha@users.noreply.github.com>
2023-02-19 19:21:56 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ FILE_NAME = '123.xlsx'#Имя файла(должен находиться в т
 
 FIELD_NAME = 'ITEM'#Поле для поиска
 
-TOKEN = '1619442200:AAHQEb78I_wabHoBYD2fIS0u5Wb8ubMW3o0'
+TOKEN = '6021283271:AAHf99148Uh0qXQ2eweCNFb43ceLw3T6_Ts'
 
 SHEET = 'Лист1'
